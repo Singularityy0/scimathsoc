252f1b38817e6506f0d75b47c6fca9a2f3c70d94
Singularityy0 <patrickbatemanconfession@gmail.com>
2025-08-04 01:28:00 +0530

    update eevents

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -27,24 +27,24 @@ const Events = () => {
         >
           <div className="event-card">
             <div className="event-date">
-              <span className="day">15</span>
-              <span className="month">Dec</span>
+              <span className="day">5</span>
+              <span className="month">Aug</span>
               <span className="year">2025</span>
             </div>
             <div className="event-content">
               <h3>Upcoming</h3>
               <p className="event-description">
-                We'll be updating events soon.
+                SciMathSoc’s Intro Session at IIT Kanpur welcomes new students, showcasing the society’s activities, workshops, and competitions to spark interest in science and mathematics.
               </p>
               <div className="event-meta">
-                <span className="event-category workshop">Workshop</span>
+                <span className="event-category Intro">Scimathsoc Intro session</span>
                 <span className="event-time">
                   <Clock size={16} />
-                  2:00 PM - 5:00 PM
+                  7:00 PM - 9:00 PM
                 </span>
                 <span className="event-location">
                   <MapPin size={16} />
-                  [location], IIT Kanpur
+                  L7, Lecture Hall Complex, IIT Kanpur
                 </span>
               </div>
               <div className="event-status">
